bd937d509c8a1d84d568eb45bd866ea7b8a8350f
AbrihaDavid <abrihadavid@gmail.com>
2021-10-22 19:43:48 +0200

    Task: "Számsorozat" done

---
+++ training-solutions/src/main/java/aslist/Sequence.java
@@ -0,0 +1,20 @@
+package aslist;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Sequence {
+
+    public static void main(String[] args) {
+
+        List<Integer> elements = new ArrayList<>(Arrays.asList(2,2));
+        System.out.println(elements);
+
+        for (int i = 0; i<4; i++){
+            elements.add(elements.get(i)*elements.get(i+1));
+        }
+        System.out.println(elements);
+        System.out.println(elements.size());
+    }
+}
