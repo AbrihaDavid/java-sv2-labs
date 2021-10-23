982c653b3ac2c424af81acde2edeab133075ea70
AbrihaDavid <abrihadavid@gmail.com>
2021-10-23 11:22:48 +0200

    Task: "Köszönj!" done

---
+++ training-solutions/src/main/java/finalmodifier/Gentleman.java
@@ -0,0 +1,10 @@
+package finalmodifier;
+
+public class Gentleman {
+
+    private static final String MESSAGE_PREFIX = "Hi ";
+
+    public String sayHello(String name){
+        return MESSAGE_PREFIX + name;
+    }
+}
