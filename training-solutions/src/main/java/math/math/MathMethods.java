00483ed0de009e12a112d76c423fa3f947914cfb
AbrihaDavid <abrihadavid@gmail.com>
2021-10-23 12:10:34 +0200

    Task: "A Math osztály kipróbálása" done

---
+++ training-solutions/src/main/java/math/math/MathMethods.java
@@ -0,0 +1,22 @@
+package math.math;
+
+public class MathMethods {
+
+    public static void main(String[] args) {
+
+        System.out.println(Math.max(20,10));
+        System.out.println(Math.min(20,10));
+        System.out.println(Math.PI);
+        System.out.println(Math.round(Math.PI));
+        System.out.println(Math.round(10.20));
+        System.out.println(Math.abs(-300.3));
+        System.out.println(Math.negateExact(1));
+        System.out.println(Math.addExact(10,20));
+        System.out.println(Math.subtractExact(10,20));
+        System.out.println(Math.multiplyExact(10,10));
+        System.out.println(Math.pow(2.2,2.2));
+        System.out.println(Math.incrementExact(5));
+        System.out.println(Math.decrementExact(1));
+        System.out.println(Math.random());
+    }
+}
