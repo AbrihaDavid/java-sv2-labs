8db5447d133a7674a3afd1f18692dc1a429d7a2b
AbrihaDavid <abrihadavid@gmail.com>
2021-10-24 18:10:33 +0200

    Task: "Kutyák" done

---
+++ training-solutions/src/main/java/references/dogs/Dog.java
@@ -0,0 +1,45 @@
+package references.dogs;
+
+public class Dog {
+
+    private String name;
+    private int age;
+    private String colour;
+
+    public Dog(String name, int age, String colour) {
+        this.name = name;
+        this.age = age;
+        this.colour = colour;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+    @Override
+    public String toString() {
+        return "The name of the dog is " + name +"\n" +
+                "He/she is " + age + " years old" + "\n" +
+                "His/her colour is " + colour;
+    }
+}
