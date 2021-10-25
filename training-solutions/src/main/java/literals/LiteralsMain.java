1328a7edeaba158db548714b2e8d1c015d7e7ba6
David <abrihadavid@gmail.com>
2021-10-25 13:53:17 +0200

    Tasks for "Literálok" done

---
+++ training-solutions/src/main/java/literals/LiteralsMain.java
@@ -0,0 +1,22 @@
+package literals;
+
+public class LiteralsMain {
+
+    public static void main(String[] args) {
+
+        System.out.println("1"+""+"2");
+
+        double quotient = 3/4D;
+        System.out.println(quotient);
+
+        long big = 3_244_444_444L;
+        System.out.println(big);
+
+        String word = "title".toUpperCase();
+        System.out.println(word);
+
+        System.out.println(Integer.toBinaryString(1));
+        System.out.println(Integer.toBinaryString(-2));
+
+    }
+}
