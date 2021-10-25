24e2c2626d3d5708539da8b8317b06569ac7599f
AbrihaDavid <abrihadavid@gmail.com>
2021-10-25 21:00:42 +0200

    Task: "A Naprendszer bolygói" done

---
+++ training-solutions/src/main/java/enumtype/solarsystem/SolarSystemMain.java
@@ -0,0 +1,9 @@
+package enumtype.solarsystem;
+
+public class SolarSystemMain {
+
+    public static void main(String[] args) {
+
+        System.out.println(SolarSystem.valueOf("JUPITER").getNumberOfMoons());
+    }
+}
