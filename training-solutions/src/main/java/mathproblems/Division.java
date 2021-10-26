6dafab1d711cab9963fedcd28da055e14e976abd
AbrihaDavid <abrihadavid@gmail.com>
2021-10-26 14:42:53 +0200

    Task: "Számok indexe" done

--- training-solutions/src/main/java/mathproblems/Division.java
+++ training-solutions/src/main/java/mathproblems/Division.java
@@ -10,10 +10,25 @@ public class Division {
         }
     }
 
+
+    public void getRightNumbers (int[] numbers){
+        for (int i =0; i< numbers.length; i++){
+            if (i==0){
+                continue;
+            }
+            if (numbers[i] % i ==0){
+                System.out.print(numbers[i] + " ");
+            }
+        }
+    }
+
+
     public static void main(String[] args) {
 
         Division division = new Division();
 
         division.getDivisors(34);
+        int [] nums = {2, 5, 23, 56, -36, 45, 78, 14, -8, 4, 10};
+        division.getRightNumbers(nums);
     }
 }
