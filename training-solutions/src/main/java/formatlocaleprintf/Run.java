d997004dd079abf948d45961c5699219c917dd5c
AbrihaDavid <abrihadavid@gmail.com>
2021-10-29 17:35:21 +0200

    Task: "Futás!" done

---
+++ training-solutions/src/main/java/formatlocaleprintf/Run.java
@@ -0,0 +1,45 @@
+package formatlocaleprintf;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Run {
+
+    private String name;
+    private List<Double> kms;
+
+    public Run(String name, List<Double> kms) {
+        this.name = name;
+        this.kms = kms;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Double> getKms() {
+        return kms;
+    }
+
+    public void addRun (double km){
+        kms.add(km);
+    }
+
+    public void printFormattedRunText(){
+        LocalDate localDate = LocalDate.now();
+        System.out.printf("Kedves %s! A mai dátum: %s. Ezen a héten ez a(z) %d futásod.",getName(),localDate.toString(),kms.size());
+        System.out.println();
+        System.out.printf("Most %.2f km-t futottál. Csak így tovább!",getKms().get(kms.size()-1));
+    }
+
+    public static void main(String[] args) {
+
+        List<Double> kms = new ArrayList<>(Arrays.asList(14.5,12.4,5.5));
+        Run run = new Run("Laci",kms);
+        run.addRun(10.35);
+        run.addRun(5);
+        run.printFormattedRunText();
+    }
+}
