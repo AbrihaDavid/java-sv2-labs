ffd2c35c2036a91b24d111df21b2f876208bbe8d
AbrihaDavid <abrihadavid@gmail.com>
2021-11-14 11:30:08 +0100

    Task: "Generált tömb" done

---
+++ training-solutions/src/main/java/introexceptiontrace/NumbersMain.java
@@ -0,0 +1,10 @@
+package introexceptiontrace;
+
+public class NumbersMain {
+
+    public static void main(String[] args) {
+
+        Change change = new Change();
+        change.changeNumbers();
+    }
+}
