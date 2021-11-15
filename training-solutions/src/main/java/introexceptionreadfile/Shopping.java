ee3c594a979b96f32ab2ab518e078fd2b024e4df
AbrihaDavid <abrihadavid@gmail.com>
2021-11-15 15:11:28 +0100

    Task: "Bevásárlólista" done

---
+++ training-solutions/src/main/java/introexceptionreadfile/Shopping.java
@@ -0,0 +1,21 @@
+package introexceptionreadfile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Shopping {
+
+    public static void main(String[] args) {
+
+        try {
+            List<String> fileContent = Files.readAllLines(Paths.get("shoppinglist.txt"));
+            System.out.println(fileContent);
+        }
+        catch (IOException ioe){
+            System.out.println("Can not read file");
+            ioe.printStackTrace();
+        }
+    }
+}
