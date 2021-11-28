ca31d6b610078f1b257c99893eb8dbf744ed67db
AbrihaDavid <abrihadavid@gmail.com>
2021-11-28 14:28:46 +0100

    Task: "Bicikli apróhirdetés" done

---
+++ training-solutions/src/main/java/constructoroverloading/advertisement/BikeAdvertisement.java
@@ -0,0 +1,33 @@
+package constructoroverloading.advertisement;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BikeAdvertisement {
+
+    private String description;
+    private int price;
+    private List<String> extras = new ArrayList<>();
+    private String brand;
+    private String serialNumber;
+
+    public BikeAdvertisement(String description, int price) {
+        this.description = description;
+        this.price = price;
+    }
+
+    public BikeAdvertisement(String description, int price, String brand) {
+        this(description,price);
+        this.brand = brand;
+    }
+
+    public BikeAdvertisement(String description, int price, List<String> extras, String brand) {
+        this(description,price,brand);
+        this.extras = extras;
+    }
+
+    public BikeAdvertisement(String description, int price, List<String> extras, String brand, String serialNumber) {
+        this(description,price,extras,brand);
+        this.serialNumber = serialNumber;
+    }
+}
